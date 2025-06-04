a35f0d71f1fffd310502f0c14ca76c4932db62a5
1-pluto1 <pluto.123452022@outlook.com>
2025-06-04 22:35:47 +0800

    vault backup: 2025-06-04 22:35:47

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
 import torch
 import torch.nn as nn
+import math
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=1024):
+        super().__init__()
+        self.d_model = d_model
+        pe = torch.zeros(max_len, d_model)
+        for pos in range(max_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = math.cos(pos / 10000 ** (2 * (i + 1) / d_model))
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x * math.sqrt(self.d_model)
+        seq_len = x.size(1)
+        x = x + self.pe[:, :seq_len, :]
+        return x
+
